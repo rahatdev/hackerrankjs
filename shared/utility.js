f8ac06676fefc3283fd5540a166518046f1da2b0
rahat <rahat.forja@gmail.com>
2017-11-29 16:29:09 -0600

    added toDecimal function to utility

--- shared/utility.js
+++ shared/utility.js
@@ -5,6 +5,21 @@ const toBinary = (i)=>{
     return toBinary(parseInt(i/2)) + i%2
 }
 
+const toDecimal = (binary) => {
+    let arr = binary.split('')
+    let num = 0
+    let maxBase = arr.length - 1
+    arr.forEach(b => {
+        if(maxBase <= 1) {
+            if(b === 1) num += 1
+        } else {
+            num += 2 ** (maxBase * b)
+            maxBase --
+        }
+    })
+    return num
+}
+
 module.exports = {
     toBinary
 }
